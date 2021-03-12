2c927052088eefae1f41ba3d8e773447c2bba630
Guilherme <guiihh532@gmail.com>
2021-03-12 11:06:52 -0300

    implement Service and repository

--- src/main/java/com/gomlek/coursemc/resources/CategoryResource.java
+++ src/main/java/com/gomlek/coursemc/resources/CategoryResource.java
@@ -1,31 +1,36 @@
 package com.gomlek.coursemc.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gomlek.coursemc.entities.Category;
+import com.gomlek.coursemc.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Category> findAll() {
-		
-		Category cat1 = new Category (1, "Computing");
-		Category cat2 = new Category (2, "Desk");
-		
-		List<Category> list = new ArrayList<>();
-		
-		list.add(cat1);
-		list.add(cat2);
-		
-		return list;
+	@Autowired
+	private CategoryService service;
+
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+
+		List<Category> list = service.findAll();
+
+		return ResponseEntity.ok().body(list);
 	}
-	
-		
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id) {
+		Category obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
 }
