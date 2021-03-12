7afca61d17e3fd6665decd8ac90738d82bd28ed9
Guilherme <guiihh532@gmail.com>
2021-03-12 10:04:05 -0300

    testing first entities class - category

--- src/main/java/com/gomlek/coursemc/resources/CategoryResource.java
+++ src/main/java/com/gomlek/coursemc/resources/CategoryResource.java
@@ -1,16 +1,30 @@
 package com.gomlek.coursemc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gomlek.coursemc.entities.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String findAll() {
-		return "REST está funcionando";
+	public List<Category> findAll() {
+		
+		Category cat1 = new Category (1, "Computing");
+		Category cat2 = new Category (2, "Desk");
+		
+		List<Category> list = new ArrayList<>();
+		
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 	
 		
